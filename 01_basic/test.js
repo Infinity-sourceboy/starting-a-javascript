32b82c9fecb295dafdbf05e70d20ad797fd879af
Infinity-sourceboy <97177843+Infinity-sourceboy@users.noreply.github.com>
2024-01-17 16:05:43 +0000

    Math and number

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,42 @@
 console.log("rajesh")
+// number and math
+console.log(400);
+const balanace = 500;
+const score = new Number(500);
+console.log(score);
+console.log(typeof balanace.toString());
+console.log(score.toFixed(2));
+
+let num = 12.4578;
+console.log(num.toPrecision(4));
+
+let num1 = 124077798;
+console.log(num1.toLocaleString('en-IN'));
+
+// *************************MATH lab ********************
+console.log(Math);
+console.log(Math.abs(-3));
+console.log(Math.round(2.6));
+console.log(Math.round(4.2));
+
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4 ,2 ,8 ,10));
+console.log(Math.max(10,5 ,7 ,1,5));
+console.log(Math.sqrt(3.2));
+console.log(Math.pow(2));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+let min = 10
+let max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
+
+
+
+
+
+
